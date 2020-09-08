33442316e4a41ce541a7948ca6269f4706537553
sridhar-reddy <sridharreddy.93953@gmail.com>
2020-09-08 22:14:10 +0530

    webpack dev server installed.

--- webpack.common.js
+++ webpack.common.js
@@ -2,14 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-    entry : "./src/index.js",
-    output : {
-        filename : "main.[contentHash].js",
-        path : path.resolve(__dirname, "dist")
-    },
-    mode : "development",
-    devtool : "none",
-    
+    entry : "./src/index.js",    
     module : {
         rules : [
             {
