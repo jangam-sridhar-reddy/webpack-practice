4d193123cf7cc81dd14a931125991dea90799dd0
sridhar-reddy <sridharreddy.93953@gmail.com>
2020-09-08 21:44:28 +0530

    html webpack plugin added.

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,35 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
     entry : "./src/index.js",
     output : {
-        filename : "main.js",
+        filename : "main.[contentHash].js",
         path : path.resolve(__dirname, "dist")
     },
     mode : "development",
     devtool : "none",
+    
     module : {
         rules : [
             {
-                test : /\.css$/,
-                use : ["style-loader", "css-loader"]
+                test : /\.scss$/,
+                use : [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ]
             }
         ]
-    }
+    },
+    plugins : [ 
+        new HtmlWebpackPlugin(
+            {
+                template : "./src/index.html"
+            }
+        )
+    ],
 }
